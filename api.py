ea900276282cf29907cf7a00936c152c7556c189
Eren Tor <142098030+ErenT647@users.noreply.github.com>
2025-12-09 09:29:20 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+from datetime import datetime, timedelta, timezone
+import pytz
 
 def get_user_rating(user):
     return get_user_info(user)["rank"]
@@ -14,6 +15,13 @@ def get_user_pending(user):
 def get_templates(idx):
     pass
 
+def get_submissions(cont_info):
+    #require:
+    #handle, contest id, number of subs
+
+    req = requests.get(f"https://codeforces.com/api/user.status?handle={cont_info["handle"]}&count={cont_info["subs"]}")
+    return req.text
+
 def get_contest_progress(sub):
     #require: 
     #handle, contest id
@@ -24,7 +32,6 @@ def get_contest_progress(sub):
     infoq = info["problemResults"]
 
     #print(infoq)
-
     cnt = 0
     rejected = []
     i = 0
@@ -52,13 +59,22 @@ def get_contest_progress(sub):
 
 def get_user_info(user):
     req = requests.get(f"https://codeforces.com/api/user.info?handles={user};&checkHistoricHandles=false")
+    print(req.text)
     info = req.json()["result"][0]
+    file = open("images/user/avatar.png", "wb")
+    file.write(requests.get(info["avatar"]).content)
     return {
         "rating": info["rating"],
         "rank": info["rank"],
         "maxRank": info["maxRank"],
-        "organization": info["organization"]
+        "organization": info["organization"],
+        "avatar": file
     }
+
+#req = requests.get("https://userpic.codeforces.org/no-avatar.jpg")
+#with open("images/user/avatar.png", "wb") as file:
+    file.write(req.content)
+
 
 def get_user_subs(user):
     req = requests.get(f"https://codeforces.com/api/user.status?handle={user}")
@@ -70,9 +86,10 @@ def get_user_subs(user):
 
     curr_time = datetime.now()
     last = curr_time
+    last.replace(hour=0, minute = 0, second=0, microsecond=0)
     curr_streak = 1
     for s in info:
-        sub_time = datetime.fromtimestamp(int(s["creationTimeSeconds"]), tz=datetime.timezone(timedelta(hours=-6)))
+        sub_time = datetime.fromtimestamp(int(s["creationTimeSeconds"]), tz=timezone(timedelta(hours=-6)))
         if s["verdict"] != "OK":
             continue
                 
@@ -81,6 +98,9 @@ def get_user_subs(user):
             month_solved += 1
 
         max_streak = max(max_streak, curr_streak)
+        
+        timedelta(sub_time, datetime(year = 1990, month = 12, day = 2, second = 1, minute = 1, microsecond = 0))
+        continue
 
         if timedelta(sub_time, curr_time).days < 1:
             streak = 1
@@ -95,10 +115,12 @@ def get_user_subs(user):
 
         if timedelta(sub_time, last).days > 1:
             last = sub_time
+            last.replace(hours=0, minute = 0, second=0, microsecond=0)
 
         curr_streak += 1
         
         max_streak = max(max_streak, curr_streak)
+    
     return {
         "maxStreak": max_streak,
         "streak": streak,
@@ -106,3 +128,5 @@ def get_user_subs(user):
         "problemsSolvedInLastMonth": month_solved
     }
 
+
+print(get_user_subs("353v1"))
